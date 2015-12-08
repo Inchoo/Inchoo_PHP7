7c97368e58fa8cdb43e1f63f6ca3cf60de6726b0
Ivan Čurdinjaković <ivan.curdinjakovic@inchoo.net>
2015-12-08 08:54:53 +0100

    Fixed 2 more PHP7 incompatibilities.

---
+++ app/code/local/Inchoo/Model/Export/Entity/Customer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created on: 08.12.15.
+ * Inchoo d.o.o.
+ * @author Ivan Čurdinjaković <ivan.curdinjakovic@inchoo.net>
+ */ 
+class Inchoo_PHP7_Model_Export_Entity_Customer extends Mage_ImportExport_Model_Export_Entity_Customer
+{
+    public function filterAttributeCollection(Mage_Eav_Model_Resource_Entity_Attribute_Collection $collection)
+    {
+        foreach (parent::filterAttributeCollection($collection) as $attribute) {
+            if (!empty($this->_attributeOverrides[$attribute->getAttributeCode()])) {
+                $data = $this->_attributeOverrides[$attribute->getAttributeCode()];
+
+                if (isset($data['options_method']) && method_exists($this, $data['options_method'])) {
+                    $data['filter_options'] = $this->{$data['options_method']}();
+                }
+                $attribute->addData($data);
+            }
+        }
+        return $collection;
+    }
+}
