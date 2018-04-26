c077c7da09e8d8dc8d5d5fe8402367e44a2eb521
Ivan Weiler <ivan.weiler@inchoo.net>
2018-04-26 13:53:42 +0200

    7.2 testing, removed old autoloader from functions.php, EE encryptor check, module version increased.

--- app/code/local/Inchoo/PHP7/Helper/Data.php
+++ app/code/local/Inchoo/PHP7/Helper/Data.php
@@ -6,9 +6,41 @@ class Inchoo_PHP7_Helper_Data extends Mage_Core_Helper_Data
     protected $_moduleName = 'Mage_Core';
 
     /**
+     * @return Mage_Core_Model_Encryption
+     */
+    public function getEncryptor()
+    {
+        if ($this->_encryptor === null) {
+
+            $encryptionModel = (string)Mage::getConfig()->getNode(self::XML_PATH_ENCRYPTION_MODEL);
+
+            /**
+             * PHP 7.1 fix.
+             * EE only, switch default EE encryptor to Inchoo encryption model to resolve missing constants.
+             *
+             * (this is here to avoid major problems when someone is using Inchoo_PHP7 for CE on EE)
+             */
+            if($encryptionModel == 'Enterprise_Pci_Model_Encryption') {
+                $encryptionModel = 'Inchoo_PHP7_Model_EncryptionEE';
+            }
+            //
+
+            if ($encryptionModel) {
+                $this->_encryptor = new $encryptionModel;
+            } else {
+                $this->_encryptor = Mage::getModel('core/encryption');
+            }
+
+            $this->_encryptor = new $encryptionModel;
+            $this->_encryptor->setHelper($this);
+        }
+        return $this->_encryptor;
+    }
+
+    /**
      * Decodes the given $encodedValue string which is encoded in the JSON format
      *
-     * Overridden to prevent exceptions in json_decode
+     * PHP 7.0 fix, overridden to prevent exceptions in json_decode
      *
      * @param string $encodedValue
      * @param int $objectDecodeType
